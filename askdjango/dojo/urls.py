94b2259d382bf4e61b09d207924f135e706c8b10
zinns58 <zinns58@gmail.com>
2018-12-21 01:29:40 +0900

    PostViewSet을 Router를 통해 URLConf에 등록

--- askdjango/dojo/urls.py
+++ askdjango/dojo/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
 
 
 app_name = 'dojo'
 
+router = DefaultRouter()
+router.register(r'post', views.PostViewSet) # 2개의 URL을 처리하는 뷰함수를 만들어서 등록
+
 urlpatterns = [
-    path('post', views.post_list),
-    path('post/<int:pk>', views.post_detail),
+    path('', include(router.urls)),
 ]
--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,16 +7,3 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-# get
-post_list = PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
-
-post_detail = PostViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
