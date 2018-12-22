a9e38fa3c75583c32e72e18e10e2b35a21679f3f
zinns58 <zinns58@gmail.com>
2018-12-23 00:20:57 +0900

    SearchFilter를 통한 Post 필터링

--- askdjango/ep04/admin.py
+++ askdjango/ep04/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'updated_at')
+    search_fields = ['title']
--- askdjango/ep06/views.py
+++ askdjango/ep06/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from ep04.models import Post
 from ep04.serializers import PostSerializer
@@ -6,13 +7,22 @@ from ep04.serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
 
-    # class 내에 queryset에 조건을 추가할때 사용.
-    def get_queryset(self):
-        qs = super().get_queryset()
-        if self.request.user.is_authenticated:
-            qs = qs.filter(author=self.request.user)
-        else:
-            qs = qs.none()  # empty result
-        qs = qs.filter(title__icontains='1')
-        return qs
+
+    ## class 내에 queryset에 조건을 추가할때 사용.
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #
+    #     # GET 인자에서 search 값으로 필터링
+    #     # search = self.request.GET.get('search', '')
+    #     # if search:
+    #     #     qs = qs.filter(title__icontains=search)
+    #
+    #     if self.request.user.is_authenticated:
+    #         qs = qs.filter(author=self.request.user)
+    #     else:
+    #         qs = qs.none()  # empty result
+    #     qs = qs.filter(title__icontains='1')
+    #     return qs
